c7556830510ed65b5cf36cfee76984f852095f48
Gilberto Villa <ingilniero@gmail.com>
2013-03-09 13:08:43 -0600

    Added 'About' Activity

--- src/com/example/asteroides/Asteroides.java
+++ src/com/example/asteroides/Asteroides.java
@@ -1,8 +1,10 @@
 package com.example.asteroides;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class Asteroides extends Activity {
 
@@ -18,6 +20,11 @@ public class Asteroides extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.asteroides, menu);
         return true;
+    }
+    
+    public void lanzarAcercaDe(View view){
+    	Intent i = new Intent(this, AcercaDe.class);
+    	startActivity(i);
     }
     
 }

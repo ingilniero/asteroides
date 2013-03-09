bd04b9ff6418d86e00d796228462e6e52a130673
Gilberto Villa <ingilniero@gmail.com>
2013-03-09 13:28:12 -0600

    Finish the app when "Exit" is clicked

--- src/com/example/asteroides/Asteroides.java
+++ src/com/example/asteroides/Asteroides.java
@@ -5,15 +5,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Asteroides extends Activity {
-
+	private Button btnSalir;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        btnSalir = (Button)findViewById(R.id.btn_salir);
+        btnSalir.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View view) {
+				lanzarSalir();
+			}
+        });
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -27,4 +36,7 @@ public class Asteroides extends Activity {
     	startActivity(i);
     }
     
+    public void lanzarSalir() {
+    	finish();
+    }
 }

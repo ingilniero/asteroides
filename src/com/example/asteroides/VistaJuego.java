5b8d646e9fb144c17adef63c9a118e8bad03f38c
Gilberto Villa <ingilniero@gmail.com>
2013-03-12 21:25:13 -0600

    Spaceship added to the Game View

--- src/com/example/asteroides/VistaJuego.java
+++ src/com/example/asteroides/VistaJuego.java
@@ -12,6 +12,12 @@ public class VistaJuego extends View {
 	private Vector<Grafico> asteroides;
 	private int numAsteroides = 5;
 	private int numFragmentos = 3;
+	private Grafico nave;
+	private int giroNave;
+	private float acelarcionNave;
+	
+	private static final int PASE_GIRO_NAVE = 5;
+	private static final float PASO_ACELERACION_NAVE = 0.5f;
 	
 	public VistaJuego(Context context, AttributeSet attrs) {
 		
@@ -20,8 +26,11 @@ public class VistaJuego extends View {
 		Drawable drawableNave, drawableAsteroide, drawableMisil;
 		
 		drawableAsteroide = context.getResources().getDrawable(R.drawable.asteroide1);
+		drawableNave = context.getResources().getDrawable(R.drawable.nave);
 		
+		nave = new Grafico(this, drawableNave);
 		asteroides = new Vector<Grafico>();
+		
 		
 		for (int i = 0; i < numAsteroides; i++){
 			Grafico asteroide = new Grafico(this, drawableAsteroide);
@@ -40,15 +49,20 @@ public class VistaJuego extends View {
 	{
 		super.onSizeChanged(ancho, alto, ancho_anter, alto_anter);
 		
+		nave.setPosX(ancho / 2);
+		nave.setPosY(alto / 2);
+		
 		for(Grafico asteroide: asteroides){
-			asteroide.setPosX(Math.random() * (ancho - asteroide.getAncho()));
-			asteroide.setPosY(Math.random() * (alto - asteroide.getAlto()));
+				asteroide.setPosX(Math.random() * (ancho - asteroide.getAncho()));
+				asteroide.setPosY(Math.random() * (alto - asteroide.getAlto()));				
 		}
 	}
 	
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
+		
+		nave.dibujarGrafico(canvas);
 		
 		for(Grafico asteroide: asteroides){
 			asteroide.dibujarGrafico(canvas);

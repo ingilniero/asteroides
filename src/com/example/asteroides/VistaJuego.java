54183b68345f8e965bde85303a45f0419811ded3
Gilberto Villa <ingilniero@gmail.com>
2013-03-13 21:18:14 -0600

    Movement of asteroids added

--- src/com/example/asteroides/VistaJuego.java
+++ src/com/example/asteroides/VistaJuego.java
@@ -14,10 +14,14 @@ public class VistaJuego extends View {
 	private int numFragmentos = 3;
 	private Grafico nave;
 	private int giroNave;
-	private float acelarcionNave;
+	private float acelaracionNave;
 	
 	private static final int PASE_GIRO_NAVE = 5;
 	private static final float PASO_ACELERACION_NAVE = 0.5f;
+	
+	private ThreadJuego thread = new ThreadJuego();
+	private static int PERIODO_PROCESO = 50;
+	private long ultimoProceso = 0;
 	
 	public VistaJuego(Context context, AttributeSet attrs) {
 		
@@ -44,6 +48,31 @@ public class VistaJuego extends View {
 		}
 	}
 	
+	synchronized protected void actualizaFisica() {
+		long ahora = System.currentTimeMillis();
+		
+		if ( ultimoProceso + PERIODO_PROCESO > ahora){
+			return;
+		}
+		
+		double retardo = (ahora - ultimoProceso) / PERIODO_PROCESO;
+		ultimoProceso = ahora;
+		
+		nave.setAngulo((int) (nave.getAngulo() + giroNave * retardo));
+		double nIncX = nave.getIncX() + acelaracionNave * Math.cos(Math.toRadians(nave.getAngulo())) * retardo;
+		double nIncY = nave.getIncY() + acelaracionNave * Math.cos(Math.toRadians(nave.getAngulo())) * retardo;
+		
+		if(Math.hypot(nIncX, nIncY) <= Grafico.getMaxVelocidad()){
+			nave.setIncX(nIncX);
+			nave.setIncY(nIncY);
+		}	
+		
+		nave.incrementaPos(retardo);
+		for(Grafico asteroide : asteroides){
+			asteroide.incrementaPos(retardo);
+		}
+	}
+	
 	@Override
 	protected void onSizeChanged(int ancho, int alto, int ancho_anter, int alto_anter)
 	{
@@ -58,16 +87,29 @@ public class VistaJuego extends View {
 				asteroide.setPosY(Math.random() * (alto - asteroide.getAlto()));								
 			} while(asteroide.distancia(nave) < nave.getAlto() + nave.getAncho() );
 		}
+		
+		ultimoProceso = System.currentTimeMillis();
+		thread.start();
 	}
 	
 	@Override
-	protected void onDraw(Canvas canvas) {
+	synchronized protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
 		
 		nave.dibujarGrafico(canvas);
 		
 		for(Grafico asteroide: asteroides){
 			asteroide.dibujarGrafico(canvas);
+		}
+	}
+	
+	
+	class ThreadJuego extends Thread {
+		@Override
+		public void run(){
+			while(true) {
+				actualizaFisica();
+			}
 		}
 	}
 }

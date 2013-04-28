254e1d21721d4c60f500e1629cd8724f514c4351
Gilberto Villa <ingilniero@gmail.com>
2013-04-28 09:07:32 -0500

    Add verification to check if have access to external storage

--- src/com/example/asteroides/AlmacenPuntuacionesArhivoExterno.java
+++ src/com/example/asteroides/AlmacenPuntuacionesArhivoExterno.java
@@ -9,6 +9,7 @@ import java.util.Vector;
 import android.content.Context;
 import android.os.Environment;
 import android.util.Log;
+import android.widget.Toast;
 
 public class AlmacenPuntuacionesArhivoExterno implements AlmacenPuntuaciones{
 	private static String ARCHIVO = Environment.getExternalStorageDirectory() + "/puntuaciones.txt";
@@ -21,6 +22,14 @@ public class AlmacenPuntuacionesArhivoExterno implements AlmacenPuntuaciones{
 	
 	@Override
 	public void guardarPuntuacion(int puntos, String nombre, long fecha) {
+		
+		String estado = Environment.getExternalStorageState();
+		if(!estado.equals(Environment.MEDIA_MOUNTED)){
+			Toast.makeText(context, "No se puede acceder a la memoria externa", Toast.LENGTH_LONG).show();
+			return;
+		}
+		
+		
 		try {
 			FileOutputStream f = new FileOutputStream(ARCHIVO, true);
 			String texto = puntos + " "+ nombre + "\n";
@@ -35,6 +44,14 @@ public class AlmacenPuntuacionesArhivoExterno implements AlmacenPuntuaciones{
 	@Override
 	public Vector<String> listaPuntuaciones(int cantidad) {
 		Vector<String> result = new Vector<String>();
+		
+		String estado = Environment.getExternalStorageState();
+		
+		if(!estado.equals(Environment.MEDIA_MOUNTED) && !estado.equals(Environment.MEDIA_MOUNTED_READ_ONLY)){
+			Toast.makeText(context, "No se puede acceder a la memoria externa", Toast.LENGTH_LONG).show();
+			return result;
+		}
+		
 		try{
 			FileInputStream f = new FileInputStream(ARCHIVO);
 			BufferedReader entrada = new BufferedReader(new InputStreamReader(f));
